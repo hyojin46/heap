8fbfaeccf6ce1695ac62198ab449119ab532cfd3
hyojin park <candy020406@naver.com>
2022-11-22 18:23:30 +0900

    수정

--- 자료구조과제/3.cs
+++ 자료구조과제/3.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace 자료구조과제
+public class MaxHeap
 {
-    internal class _3
+    int i,;
+    int heap_size;
+    data
+    int data_left = data * 2; //왼쪽 자식노드
+    int data_right = data * 2 + 1; //오른쪽 자식노드
+}
+    class Test
+{
+    static public void Main(string[] args)
     {
+        MaxHeap mh = new MaxHeap();
+        mh.MaxHeapAdd(10);
+        mh.MaxHeapAdd(40);
+        mh.MaxHeapAdd(30);
+        mh.MaxHeapAdd(5);
+        mh.MaxHeapAdd(12);
+        mh.MaxHeapAdd(6);
+        mh.MaxHeapAdd(15);
+        mh.MaxHeapAdd(9);
+        mh.MaxHeapAdd(60);
+        mh.MaxHeapDelete();
+
+        for (int i = 0; i < mh.MaxHeapList.Count; i++)
+        {
+            Console.Write(mh.MaxHeapList[i]);
+        }
     }
 }
+
